33b2e194c9ce4946cf5df55356c80d559953d8da
Sanjana Rao <sanjanagrao99@gmail.com>
2021-08-20 14:30:34 +0530

    Modified: Coupon Number Program and Logic

--- CouponNumber.java
+++ CouponNumber.java
@@ -2,8 +2,21 @@
 public class CouponNumber {
 
 	public static void main(String[] args) {
-		//text.
 
+		char[] charAvail="123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".toCharArray(); //Valid Coupon Codes
+		int max=100000000;
+		int randomNo=(int) (Math.random()*max);	//to generate random codes
+		StringBuffer sb=new StringBuffer();
+		
+		//generation of random Coupon Number
+		while (randomNo>0)
+		{
+			sb.append(charAvail[randomNo % charAvail.length]); 
+			randomNo /= charAvail.length;
+		}
+
+		String code=sb.toString();
+		System.out.println("Therefore the Coupon Number is: "+code);
 	}
 
 }
